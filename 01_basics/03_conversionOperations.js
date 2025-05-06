85636ea47e8ca757e1d819ef03485279789f3315
Shobhit Shukla <50shuklabasti@gmail.com>
2025-05-06 12:25:09 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+// let score="123sabc"
+ 
+// let newScore= Number(score)
+// console.log(score);
+
+// console.log(typeof score);
+// console.log(typeof newScore);
+
+let Id="123abc"
+
+let numberId=Number(Id)
+console.log(typeof Id);
+console.log(typeof stringId);
+console.log(numberId);
+
